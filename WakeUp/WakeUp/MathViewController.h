5bd798ce687cae760eb8d075ba27e9240cefcb6c
Judy Mai <judymai96@gmail.com>
2015-06-29 16:02:42 -0700

    Add MathViewController

---
+++ WakeUp/WakeUp/MathViewController.h
@@ -0,0 +1,13 @@
+//
+//  MathViewController.h
+//  WakeUp
+//
+//  Created by Judy Mai on 6/29/15.
+//  Copyright (c) 2015 BigNerdRanch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MathViewController : UIViewController
+
+@end
