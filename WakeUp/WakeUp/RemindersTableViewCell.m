2e7ceda1a51be004492002644fbea8150649138f
catherynl <catheryn@catheryn-mbp.dhcp.thefacebook.com>
2015-06-29 16:03:53 -0700

    Added RemindersViewCell

---
+++ WakeUp/WakeUp/RemindersTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  RemindersTableViewCell.m
+//  WakeUp
+//
+//  Created by Catherine Li on 6/29/15.
+//  Copyright © 2015 BigNerdRanch. All rights reserved.
+//
+
+#import "RemindersTableViewCell.h"
+
+@implementation RemindersTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
